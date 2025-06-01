d70a47db043a5968fba595190fff149a75703a8f
Murka <muratahmethanov@gmail.com>
2025-06-01 16:45:04 +0300

    added .env with keys

--- src/api/apiClient.ts
+++ src/api/apiClient.ts
@@ -1,9 +1,12 @@
 import axios from 'axios';
 
+const HASURA_ADMIN_KEY = import.meta.env.VITE_HASURA_ADMIN_KEY
+const BASE_URL = import.meta.env.VITE_BASE_API_URL
+
 export const apiClient = axios.create({
-  baseURL: 'http://localhost:8080/v1/graphql',
+  baseURL: BASE_URL,
   headers: {
     'content-Type': 'application/json',
-    'x-hasura-admin-secret': 'test123',
+    'x-hasura-admin-secret': HASURA_ADMIN_KEY,
   },
 });
