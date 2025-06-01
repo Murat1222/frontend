d597d1f12acf7240bd9010a1fa6828091ee69e3a
Murka <muratahmethanov@gmail.com>
2025-06-01 21:22:18 +0300

    added sort for labels

--- src/components/LabelList/LabelList.tsx
+++ src/components/LabelList/LabelList.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { useDeleteLabel, useLabels } from "../../hooks/useLabels"
 import Loader from "../../shared/Loader/Loader";
 import { truncateText } from "../../shared/TruncateText/TruncateText";
@@ -8,6 +9,7 @@ import styles from './styles.module.scss'
 function LabelList() {
   const {data: labels, isLoading, error} = useLabels();
   const deleteLabel = useDeleteLabel();
+  const [sortAlphabetical, setSortAlphabetical] = useState(false);
 
   if (isLoading) return <Loader />;
   if (error) return <div className={styles.error}>Error: {error.message}</div>;
@@ -16,12 +18,26 @@ function LabelList() {
     deleteLabel.mutate(id);
   };
 
+  const toggleSort = () => setSortAlphabetical(prev => !prev);
+
+  const sortedLabels = [...(labels || [])].sort((a, b) => {
+    return sortAlphabetical ? a.caption.localeCompare(b.caption) : 0;
+  });
+
   return (
     <>
-      <h1 className={styles.title}>Labels</h1>
+        <h1 className={styles.title}>Labels</h1>
       <LabelForm />
+        <div className={styles.sort_containter}>
+          <button 
+          onClick={toggleSort}
+          className={styles.sortbtn}
+          >
+            {sortAlphabetical ? 'Сбросить' : 'Сортировать по алфавиту'}
+          </button>
+        </div>
       <div className={styles.grid}>
-        {labels?.map((label : ILabel) => (
+        {sortedLabels.map((label : ILabel) => (
           <div key={label.id} className={styles.label}>
             <h3 className={styles.label__caption}>{truncateText(label.caption, 45)}</h3>
             <p className={styles.label__color}>{truncateText(label.color, 45)}</p>
